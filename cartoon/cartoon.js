07f96290c9564f5335869d4b53581c4bf4668442
Shakhram <shakag6@gmail.com>
2023-02-25 12:15:21 -0500

    Add more house elements and draw caption on canvas

--- cartoon/cartoon.js
+++ cartoon/cartoon.js
@@ -45,17 +45,37 @@ function draw() {
   ctx.fillStyle = "#305e07";
   ctx.fillRect(0, 350, canvas.width, canvas.height);
 
+  //chimney smoke
+  let ypos = 80;
+  for (let i = 0; i < 4; i++) {
+    ctx.fillStyle = "grey";
+    ctx.lineWidth = -1;
+    ctx.beginPath();
+    ctx.arc(513, ypos, 15, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+  
+    ctx.fillStyle = "grey";
+    ctx.lineWidth = -1;
+    ctx.beginPath();
+    ctx.arc(518, ypos, 15, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+    ypos += 20;
+  }
+
   //chimney
   ctx.beginPath();
   ctx.rect(500, 150, 30, 100);
   ctx.stroke();
   ctx.fillStyle = "#361006";
   ctx.fill();
-  //chimney smoke
 
   //house base
   ctx.beginPath();
-  ctx.rect(500, 200, 200, 200);
+  ctx.rect(500, 200, 200, 150);
   ctx.stroke();
   ctx.fillStyle = "#dbd1ad";
   ctx.fill();
@@ -69,7 +89,33 @@ function draw() {
   ctx.fillStyle = "#631502";
   ctx.fill();
 
-  //window
-  
+  //attic window
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(600, 155, 25, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  //base window
+  ctx.beginPath();
+  ctx.rect(515, 250, 70, 60);
+  ctx.stroke();
+  ctx.fillStyle = "white";
+  ctx.fill();
+
   //door
+  ctx.beginPath();
+  ctx.rect(600, 250, 70, 100);
+  ctx.stroke();
+  ctx.fillStyle = "grey";
+  ctx.fill();
+
+  //trees for loop
+
+  //pond
+
+  //garden for loop
+
+  ctx.font = "48px serif";
+  ctx.strokeText("House", 50, 400);
 }
