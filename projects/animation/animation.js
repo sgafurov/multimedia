558564b59c681615dcb2d71b6e841248d477af3b
Shakhram <shakag6@gmail.com>
2023-03-15 20:53:50 -0400

    Animate cars with speech bubbles on a city road

--- projects/animation/animation.js
+++ projects/animation/animation.js
@@ -1,35 +1,50 @@
 document.addEventListener("DOMContentLoaded", draw);
 
 function draw() {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
-  ghostCanvas = document.createElement("canvas"); //holds onto the original image
-  ghostCtx = ghostCanvas.getContext("2d");
-  ghostCanvas.width = canvas.width;
-  ghostCanvas.height = canvas.height;
+  var car1 = new Image();
+  car1.src = './green-car.png';
+  car1.onload = animate;
 
-  //store bee's quotes in an array and display text while looping thru array
+  var car2 = new Image();
+  car2.src = './blue-car.png';
+  car2.onload = animate;
 
-  bee1 = new Image();
-  bee1.src = "./bee.png";
+  var text_bubble = new Image();
+  text_bubble.src = './text-bubble.png';
+  text_bubble.onload = animate;
 
-  bee1.onload = () => {
-    ctx.drawImage(bee1, 0, 0, 400, 300); //initial draw of the image at coords (0, 100) and dimensions 400x300
-    ghostCtx.drawImage(canvas, 0, 0, 400, 300);
-    requestAnimationFrame(moveRight);
-  };
+  x_pos_car1 = 0;
+  x_pos_car2 = canvas.width;
+  
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
-  var x = 0;
-  function moveRight() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //resets canvas so the image doesn't create a streak
-    ctx.drawImage(ghostCanvas, x, 100, canvas.width, canvas.height);
-    requestAnimationFrame(moveRight);
-    x += 2;
-    if (x == canvas.width) {
-      //loops the image once it has exited the canvas window
-      x = 0;
+    ctx.drawImage(car2, x_pos_car2, 350, 350, 200); // draw image in the back
+    ctx.drawImage(text_bubble, x_pos_car2-50, 280, 200, 100); // draw image in the back
+    ctx.font = "italic 30px Helvetica";
+    ctx.fillStyle = "blue";
+    ctx.fillText("beep beep", x_pos_car2-30, 320);
+
+    ctx.drawImage(car1, x_pos_car1, 410, 450, 300); // draw front image to overlap the image in the back
+    ctx.drawImage(text_bubble, x_pos_car1+300, 380, 200, 100); // draw front image to overlap the image in the back
+
+    ctx.font = "30px Helvetica";
+    ctx.fillStyle = "red";
+    ctx.fillText("Hey Dan", x_pos_car1+330, 420);
+
+    x_pos_car1 += 4;
+    x_pos_car2 -= 4;
+    if (x_pos_car1 < canvas.width) {
+      requestAnimationFrame(animate); // loop
+    }
+    if (x_pos_car1 >= canvas.width) {
+      // reached end of canvas
+      x_pos_car1 = 0; // reset position
+      x_pos_car2 = canvas.width;
+      requestAnimationFrame(animate);
     }
   }
-
 }
